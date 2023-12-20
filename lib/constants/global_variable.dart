f8cfae6dbca417588454c7861dde265cc76adadb
aman kumar <kumaraman187187@gmail.com>
2023-12-20 17:02:29 +0530

    initial commit aman

---
+++ lib/constants/global_variable.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GlobalVariable{
+
+  static const SecondaryColor = Color.fromRGBO(238, 91, 110, 100);
+  static const backgroundColor = Colors.grey;
+    static const Color greyBackgroundColor = Color(0xffe73737);
+  static var splace1 = Colors.cyan[800];
+  static const unselectedNavBarColor = Colors.black87;
+  static const elevatedbuttoncolor = Colors.orange;
+
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:bit_go/constants/assets_images.dart';
+import 'package:bit_go/screen/AuthScreen/Login.dart';
+import 'package:bit_go/screen/SplaceScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+
+  bool showsplase = true;
+  Leadhome(){
+    Future.delayed(Duration(seconds: 3),(){
+      setState(() {
+        showsplase = false;
+      });
+    });
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Leadhome();
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:   SplaceScreen() ,
+
+    );
+  }
+}
+
